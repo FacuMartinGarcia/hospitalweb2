361fbdc74006c05b99b3fe2a1fb28a1ac3d5a9d0
Facu Martin Garcia <estudiodesarrolladorsoftware@gmail.com>
2025-05-26 19:58:04 -0300

    Conectar a Railway
    * Establece la conexion

--- config/db.js
+++ config/db.js
@@ -1,20 +1,22 @@
 //require('dotenv').config(); 
 const { Sequelize } = require('sequelize');
 
+
 const sequelize = new Sequelize('hospital', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 });
 
 
+
 /*
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     dialect: 'mysql',
     logging: false,
   }
@@ -22,5 +24,9 @@ const sequelize = new Sequelize(
 */
 
 
-
 module.exports = sequelize;
+/*
+sequelize.authenticate()
+  .then(() => console.log('✅ Conexión exitosa a Railway'))
+  .catch(err => console.error('❌ Error de conexión:', err));
+  */
