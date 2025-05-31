6e57c82ece27f8e10434968ff917a2da0aaab834
Facu Martin Garcia <estudiodesarrolladorsoftware@gmail.com>
2025-05-31 00:47:54 -0300

    rail

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 //require('dotenv').config(); 
 const { Sequelize } = require('sequelize');
 
-
+/*
 const sequelize = new Sequelize('hospital', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 });
+*/
 
 
-/*
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -24,5 +24,5 @@ const sequelize = new Sequelize(
 sequelize.authenticate()
   .then(() => console.log('✅ Conexión exitosa a Railway'))
   .catch(err => console.error('❌ Error de conexión:', err));
-*/
+
 module.exports = sequelize;
